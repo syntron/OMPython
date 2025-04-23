463811de58e93c4042808255848e204b8c139af1
syntron <syntron@web.de>
2025-04-23 19:26:09 +0200

    [ModelicaSystem] suppress warnings
    reduce the number of warnings issued by PyCharm
    
    * unresolved references (in docstrings; due to old syntax based
      on non-class methods)
    * suppress name schema warnings - upper letters in function names

--- OMPython/ModelicaSystem.py
+++ OMPython/ModelicaSystem.py
@@ -322,6 +322,8 @@ class ModelicaSystemCmd:
         return simargs
 
 
+# TODO: use PyUnresolvedReferences?
+# noinspection PyPep8Naming, PyUnresolvedReferences
 class ModelicaSystem:
     def __init__(
             self,
