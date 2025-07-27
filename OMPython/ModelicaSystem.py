3501b1e1c3288cd0fbb5c0a8e8750b2fb068fa67
syntron <syntron@web.de>
2025-07-27 08:48:09 +0200

    Revert "[ModelicaSystem.buildModel] check if model executable exists"
    This reverts commit 5e006109b543853b2db00773955afca406737790.

--- OMPython/ModelicaSystem.py
+++ OMPython/ModelicaSystem.py
@@ -433,9 +433,6 @@ class ModelicaSystem:
         return self._work_dir
 
     def buildModel(self, variableFilter: Optional[str] = None):
-        """
-        Build the model executable.
-        """
         if variableFilter is not None:
             self._variable_filter = variableFilter
 
@@ -447,11 +444,7 @@ class ModelicaSystem:
         buildModelResult = self._requestApi("buildModel", self._model_name, properties=varFilter)
         logger.debug("OM model build result: %s", buildModelResult)
 
-        model_executable = self._getconn.omcpath(buildModelResult[0])
-        if not model_executable.is_file():
-            raise ModelicaSystemError(f"Missing model executable after buildModel(): {model_executable.as_posix()}")
-
-        xml_file = model_executable.parent / buildModelResult[1]
+        xml_file = self._getconn.omcpath(buildModelResult[0]).parent / buildModelResult[1]
         self._xmlparse(xml_file=xml_file)
 
     def sendExpression(self, expr: str, parsed: bool = True):
